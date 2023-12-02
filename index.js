07c93ec043279812fa495a1e8d34deab1593bad4
k4itrun <truchorko2222@gmail.com>
2023-12-02 16:21:18 -0500

    dpapi

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 const dpapi = require('bindings')('node-dpapi');
 
-module.exports.protectData = dpapi.protectData;
-module.exports.unprotectData = dpapi.unprotectData;
+/**
+ * Protects data using DPAPI.
+ * @param {Uint8Array} userData - The data to be protected.
+ * @param {Uint8Array | null} optionalEntropy - Optional entropy for additional protection.
+ * @param {"CurrentUser" | "LocalMachine"} scope - The scope of protection.
+ * @returns {Uint8Array} - The protected data.
+ */
+function protectData(userData, optionalEntropy, scope) {
+    return dpapi.protectData(userData, optionalEntropy, scope);
+}
+
+/**
+ * Unprotects data using DPAPI.
+ * @param {Uint8Array} encryptedData - The data to be unprotected.
+ * @param {Uint8Array | null} optionalEntropy - Optional entropy for additional protection.
+ * @param {"CurrentUser" | "LocalMachine"} scope - The scope of protection.
+ * @returns {Uint8Array} - The unprotected data.
+ */
+function unprotectData(encryptedData, optionalEntropy, scope) {
+    return dpapi.unprotectData(encryptedData, optionalEntropy, scope);
+}
+
+module.exports = {
+    protectData,
+    unprotectData
+};
--- src/node-dpapi.cpp
+++ src/node-dpapi.cpp
@@ -2,134 +2,122 @@
 #include <nan.h>
 #include <Windows.h>
 #include <dpapi.h>
-#include <functional>
 
-v8::Local<v8::String> CreateUtf8String(v8::Isolate* isolate, char* strData)
+v8::Local<v8::String> CreateUtf8String(const char* strData)
 {
-	return v8::String::NewFromUtf8(isolate, strData, v8::NewStringType::kNormal).ToLocalChecked();
+    return Nan::New<v8::String>(strData).ToLocalChecked();
 }
 
 void ProtectDataCommon(bool protect, Nan::NAN_METHOD_ARGS_TYPE info)
 {
-	v8::Isolate* isolate = info.GetIsolate();
+    Nan::HandleScope scope;
 
-	if (info.Length() != 3) {
-		isolate->ThrowException(v8::Exception::RangeError(
-			CreateUtf8String(isolate, "3 arguments are required")));
-	}
+    v8::Isolate* isolate = info.GetIsolate();
 
-	if (info[0]->IsNullOrUndefined() || !info[0]->IsUint8Array())
-	{
-		isolate->ThrowException(v8::Exception::TypeError(
-			CreateUtf8String(isolate, "First argument, data, must be a valid Uint8Array")));
-	}
+    if (info.Length() != 3)
+    {
+        Nan::ThrowError("3 arguments are required");
+        return;
+    }
 
-	if (!info[1]->IsNull() && !info[1]->IsUint8Array())
-	{
-		isolate->ThrowException(v8::Exception::TypeError(
-			CreateUtf8String(isolate, "Second argument, optionalEntropy, must be null or an ArrayBuffer")));
-	}
+    if (!info[0]->IsUint8Array())
+    {
+        Nan::ThrowTypeError("First argument, data, must be a valid Uint8Array");
+        return;
+    }
 
-	if (info[2]->IsNullOrUndefined() || !info[2]->IsString())
-	{
-		isolate->ThrowException(v8::Exception::TypeError(
-			CreateUtf8String(isolate, "Third argument, scope, must be a string")));
-	}
+    if (!info[1]->IsNull() && !info[1]->IsUint8Array())
+    {
+        Nan::ThrowTypeError("Second argument, optionalEntropy, must be null or an ArrayBuffer");
+        return;
+    }
 
-	DWORD flags = 0;
-	if (!info[2]->IsNullOrUndefined())
-	{
-		v8::String::Utf8Value strData(isolate, info[2]);
-		std::string scope(*strData);
-		if (stricmp(scope.c_str(), "LocalMachine") == 0)
-		{
-			flags = CRYPTPROTECT_LOCAL_MACHINE;
-		}
-	}
+    if (!info[2]->IsString())
+    {
+        Nan::ThrowTypeError("Third argument, scope, must be a string");
+        return;
+    }
 
-	auto buffer = node::Buffer::Data(info[0]);
-	auto len = node::Buffer::Length(info[0]);
+    DWORD flags = 0;
+    if (!info[2]->IsNullOrUndefined())
+    {
+        Nan::Utf8String scopeStr(info[2]);
+        std::string scope(*scopeStr);
+        if (stricmp(scope.c_str(), "LocalMachine") == 0)
+        {
+            flags = CRYPTPROTECT_LOCAL_MACHINE;
+        }
+    }
 
-	DATA_BLOB entropyBlob;
-	entropyBlob.pbData = nullptr;
-	if (!info[1]->IsNull())
-	{
-		entropyBlob.pbData = reinterpret_cast<BYTE*>(node::Buffer::Data(info[1]));
-		entropyBlob.cbData = node::Buffer::Length(info[1]);
-	}
+    auto buffer = node::Buffer::Data(info[0]);
+    auto len = node::Buffer::Length(info[0]);
 
-	DATA_BLOB dataIn;
-	DATA_BLOB dataOut;
+    DATA_BLOB entropyBlob;
+    entropyBlob.pbData = nullptr;
+    if (!info[1]->IsNull())
+    {
+        entropyBlob.pbData = reinterpret_cast<BYTE*>(node::Buffer::Data(info[1]));
+        entropyBlob.cbData = node::Buffer::Length(info[1]);
+    }
 
-	// initialize input data
-	dataIn.pbData = reinterpret_cast<BYTE*>(buffer);
-	dataIn.cbData = len;
+    DATA_BLOB dataIn;
+    DATA_BLOB dataOut;
 
-	bool success = false;
+    dataIn.pbData = reinterpret_cast<BYTE*>(buffer);
+    dataIn.cbData = len;
 
-	// Call either Protect or Unprotect based on the flag
-	if (protect)
-	{
-		success = CryptProtectData(
-			&dataIn,
-			nullptr, // Description string
-			entropyBlob.pbData ? &entropyBlob : nullptr,
-			nullptr, // reserved
-			nullptr, // pass null for the prompt structure
-			flags, // dwFlags
-			&dataOut);
-	}
-	else
-	{
-		success = CryptUnprotectData(
-			&dataIn,
-			nullptr, // Description string
-			entropyBlob.pbData ? &entropyBlob : nullptr,
-			nullptr, // reserved
-			nullptr, // pass null for the prompt structure
-			flags, // dwFlags
-			&dataOut);
-	}
+    bool success = false;
 
-	if (!success)
-	{
-		DWORD errorCode = GetLastError();
-		isolate->ThrowException(v8::Exception::Error(
-			CreateUtf8String(isolate, "Decryption failed. TODO: Error code")));
+    if (protect)
+    {
+        success = CryptProtectData(
+            &dataIn,
+            nullptr, 
+            entropyBlob.pbData ? &entropyBlob : nullptr,
+            nullptr,
+            nullptr,
+            flags, 
+            &dataOut);
+    }
+    else
+    {
+        success = CryptUnprotectData(
+            &dataIn,
+            nullptr, 
+            entropyBlob.pbData ? &entropyBlob : nullptr,
+            nullptr, 
+            nullptr, 
+            flags, 
+            &dataOut);
+    }
 
-		return;
-	}
+    if (!success)
+    {
+        DWORD errorCode = GetLastError();
+        Nan::ThrowError("Decryption failed. TODO: Error code");
+        return;
+    }
 
-	// Copy and free the buffer
-	auto returnBuffer = Nan::CopyBuffer(reinterpret_cast<const char*>(dataOut.pbData), dataOut.cbData).ToLocalChecked();
-	LocalFree(dataOut.pbData);
+    auto returnBuffer = Nan::CopyBuffer(reinterpret_cast<char*>(dataOut.pbData), dataOut.cbData).ToLocalChecked();
+    LocalFree(dataOut.pbData);
 
-	info.GetReturnValue().Set(returnBuffer);
+    info.GetReturnValue().Set(returnBuffer);
 }
 
-// public unsafe static byte[] Protect(byte[] userData, byte[] optionalEntropy, DataProtectionScope scope) 
 NAN_METHOD(protectData)
 {
-	ProtectDataCommon(true, info);
+    ProtectDataCommon(true, info);
 }
 
-// public unsafe static byte[] Unprotect(byte[] encryptedData, byte[] optionalEntropy, DataProtectionScope scope)
 NAN_METHOD(unprotectData)
 {
-	ProtectDataCommon(false, info);
+    ProtectDataCommon(false, info);
 }
 
 NAN_MODULE_INIT(init)
 {
-	Nan::Set(
-		target,
-		Nan::New<v8::String>("protectData").ToLocalChecked(),
-		Nan::GetFunction(Nan::New<v8::FunctionTemplate>(protectData)).ToLocalChecked());
-
-	Nan::Set(
-		target,
-		Nan::New<v8::String>("unprotectData").ToLocalChecked(),
-		Nan::GetFunction(Nan::New<v8::FunctionTemplate>(unprotectData)).ToLocalChecked());
+    Nan::SetMethod(target, "protectData", protectData);
+    Nan::SetMethod(target, "unprotectData", unprotectData);
 }
 
 #if NODE_MAJOR_VERSION >= 10
